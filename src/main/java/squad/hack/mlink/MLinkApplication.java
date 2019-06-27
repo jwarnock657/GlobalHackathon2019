f6511badb6b14636cc4dd536ddccf53754c86035
Chase Condon <chase.condon@gmail.com>
2019-06-27 10:09:47 +0100

    Better

---
+++ src/main/java/squad/hack/mlink/MLinkApplication.java
@@ -0,0 +1,13 @@
+package squad.hack.mlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MLinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/squad/hack/mlink/MLinkApplicationTests.java
@@ -0,0 +1,16 @@
+package squad.hack.mlink;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MLinkApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
