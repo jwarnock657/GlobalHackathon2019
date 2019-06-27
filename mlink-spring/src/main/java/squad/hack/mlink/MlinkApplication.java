86dc05f1962520b5483346247c4ec2794bb63244
Chase Condon <chase.condon@gmail.com>
2019-06-27 10:21:39 +0100

    Spring

---
+++ mlink-spring/src/main/java/squad/hack/mlink/MlinkApplication.java
@@ -0,0 +1,13 @@
+package squad.hack.mlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MlinkApplication.class, args);
+    }
+
+}
